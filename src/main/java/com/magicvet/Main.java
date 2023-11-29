91c22cb719224b98fdd08969d51e7af357712a7c
bogoradav <bogoradav@gmail.com>
2023-11-29 16:10:08 +0200

    REQ-01: Base authentication

---
+++ src/main/java/com/magicvet/Main.java
@@ -0,0 +1,33 @@
+package main.java.com.magicvet;
+
+import java.util.Scanner;
+
+public class Main {
+
+    static String PASSWORD = "default";
+    static Scanner SCANNER = new Scanner(System.in);
+
+    public static void main(String[] args){
+        run();
+    }
+
+    static void run() {
+        boolean accepted = false;
+        for (int i=0; i<3; i++) {
+            System.out.println(
+                    "Password: ");
+                    String input = SCANNER.nextLine();
+
+                    if (PASSWORD.equals(input)){
+                    accepted = true;
+                    break;
+                    }
+                    else {
+                System.out.println("access denied, please check your pasword");}
+
+
+            }
+        System.out.println(accepted ? "Wellcome to the magic Vet" : "Application has been blocked");
+        }
+
+    }
